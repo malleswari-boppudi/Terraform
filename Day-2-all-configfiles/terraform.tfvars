d91be7e539bac3c34dbbe02ca42a242e47fbe3b6
malleswari-boppudi <malleswariboppudi897@gmail.com>
2026-03-11 19:02:54 +0530

    day-2-code

---
+++ Day-2-all-configfiles/terraform.tfvars
@@ -0,0 +1,2 @@
+instance_type = "t2.micro"
+ami_id = "ami-02dfbd4ff395f2a1b"
---
+++ Day-2-all-configfiles/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+    default = ""
+    type = string
+}
+
+variable "ami_id" {
+    default = ""
+    type = string
+}
