e7de09e68c1694348ea15e087bc0d110e4f52578
malleswari-boppudi <malleswariboppudi897@gmail.com>
2026-03-15 08:43:15 +0530

    day-6-lockfile

--- Day-6-lockfile/backend.tf
+++ Day-6-lockfile/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "malli-bucket-14032026"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true
   }
 }
--- Day-6-lockfile/main.tf
+++ Day-6-lockfile/main.tf
@@ -5,3 +5,19 @@ resource "aws_vpc" "name" {
     }
 }
 
+resource "aws_instance" "dev" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Test-instance"
+    }  
+}
+
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.2.0/23"
+  tags = {
+    "Name" = "dev1Subnet1"
+  }
+  
+}
