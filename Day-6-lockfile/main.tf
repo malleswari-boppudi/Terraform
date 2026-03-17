226ad43429acc9aea710b25b02de35c46ca8dbe0
malleswari-boppudi <malleswariboppudi897@gmail.com>
2026-03-17 18:20:46 +0530

    day-6

--- Day-6-lockfile/main.tf
+++ Day-6-lockfile/main.tf
@@ -4,3 +4,20 @@ resource "aws_vpc" "name" {
       "Name" = "dev"
     }
 }
+
+resource "aws_instance" "dev" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Test-instance"
+    }  
+}
+
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.0.0/23"
+  tags = {
+    "Name" = "Subnet1"
+  }
+  
+}
